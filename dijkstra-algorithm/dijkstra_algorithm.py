cca872d7b3b98230c37c9df8b55c54c29bbd1fbc
Rafael Penna <rafaelgpenna@gmail.com>
2024-03-13 20:29:14 -0300

    feat: implemented dijkstra algorithm

--- dijkstra-algorithm/dijkstra_algorithm.py
+++ dijkstra-algorithm/dijkstra_algorithm.py
@@ -1,2 +1,19 @@
+from functions import set_costs, set_parents, get_lowest_cost
+
+
 def dijkstra_algorithm(graph: dict) -> int:
-    return 0
+    costs = set_costs(graph, "start")
+    node = get_lowest_cost(costs)
+    parents = set_parents(costs, node)
+    processed = []
+    while node:
+        cost = costs[node]
+        edges = graph[node]
+        for next_node in edges.keys():
+            new_cost = cost + edges[next_node]
+            if costs[next_node] > new_cost:
+                costs[next_node] = new_cost
+                parents[next_node] = node
+        processed.append(node)
+        node = get_lowest_cost(costs, processed)
+    return costs["end"]
